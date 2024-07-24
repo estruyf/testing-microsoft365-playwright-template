400f78f022a87043d6abb94b1330f3fefa8f7673
Elio Struyf <elio@struyfconsulting.be>
2024-07-24 10:00:58 +0200

    Small changes to the login setup

--- tests/login.setup.ts
+++ tests/login.setup.ts
@@ -9,18 +9,16 @@ setup("authenticate", async ({ page }) => {
   await page.goto(process.env.M365_PAGE_URL || "");
 
   const emailInput = page.locator("input[type=email]");
-  await emailInput.waitFor();
   await emailInput.click();
   await emailInput.fill(process.env.M365_USERNAME || "");
 
   await page.getByRole("button", { name: "Next" }).click();
 
   const passwordInput = page.locator("input[type=password]");
-  await passwordInput.waitFor();
   await passwordInput.click();
   await passwordInput.fill(process.env.M365_PASSWORD || "");
 
-  await page.locator("input[type=submit][value='Sign in']").click();
+  await page.locator("input[type=submit]").click();
   await page.locator("input[type=submit][value='Yes']").click();
   await page.waitForURL(process.env.M365_PAGE_URL || "");
 
